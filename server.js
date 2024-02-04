a187468abbbfa20b7db226b7bf439c5d13c5c054
justinJSwebdev <trankyhung225@gmail.com>
2024-02-04 07:44:50 +0700

    update

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const helmet = require("helmet")
 require("dotenv").config();
 const morgan = require("morgan")
 const route = require("./routes");
+const db = require("./config/db")
 
 app.use(morgan("common"))
 const PORT = process.env.PORT || 5000;
 
 
 app.use(helmet());
+
+db();
 route(app);
 
 
