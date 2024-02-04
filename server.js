04123c34d12af91468358c78fe692bab5117761d
justinJSwebdev <trankyhung225@gmail.com>
2024-02-04 19:00:55 +0700

    update

--- server.js
+++ server.js
@@ -5,17 +5,19 @@ require("dotenv").config();
 const morgan = require("morgan")
 const route = require("./routes");
 const db = require("./config/db")
-
+//??
 app.use(morgan("common"))
+//PORT
 const PORT = process.env.PORT || 5000;
 
-
+//??
 app.use(helmet());
-
+//db
 db();
+//route
 route(app);
 
-
+//server start
 app.listen(PORT, () => {
     console.log("Server running at " + PORT);
 });
