81755b4c19fa82f1de3ce10a893cfd1560e15e2c
justinJSwebdev <trankyhung225@gmail.com>
2024-02-04 19:48:24 +0700

    config error, middleware, controller

---
+++ controllers/user.ctrl.js
@@ -0,0 +1,28 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/user.model")
+
+const signUp = () => {
+
+}
+const login = () => {
+
+}
+
+const currentUser = () => {
+
+}
+
+const logOut = () => {
+
+}
+
+const forgotPassword = () => {
+
+}
+
+const refreshToken = () => {
+
+}
+module.exports = {
+    signUp, login, logOut, forgotPassword, refreshToken, currentUser
+}
